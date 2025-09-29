575c5cde05555476380a8add5e54f9dea4d09feb
Ayush <ayush.kundesar@gmail.com>
2025-09-29 09:53:43 +0530

    Add files via upload

---
+++ Operations.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << 100+10 << endl;
+    cout << 100-10 << endl;
+    cout << 100*10 << endl;
+    cout << 100/10 << endl;
+}
---
+++ Square.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+//This file is supposed to print: "Number - Square"
+int main(){
+    cout << 1 << " - " << 1*1 << endl;
+    cout << 2 << " - " << 2*2 << endl;
+    cout << 3 << " - " << 3*3 << endl;
+    cout << 4 << " - " << 4*4 << endl;
+    cout << 5 << " - " << 5*5 << endl;
+    return 0;
+}
