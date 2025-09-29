f089bf7e7a7e924b5bbba0a6329b5c2be9526a7d
Ayush <ayush.kundesar@gmail.com>
2025-09-29 09:53:03 +0530

    Add files via upload

---
+++ Comments.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //Double slashes are used for comments in single line
+    cout << 11;
+    /*Slash asterisk is used to start a multi-line comment
+    and the end with asterisk slash*/
+}
