4b6253099891063600fb26b438fe7d1795ce0089
Ayush <ayush.kundesar@gmail.com>
2025-09-29 10:28:48 +0530

    Add files via upload

---
+++ Area_Circle.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    double pi=3.14159, radius=271.2138, area;
+    area = pi*radius*radius;
+    cout << "The area of this circle is " << area << endl;
+}
---
+++ Area_Rectangle.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int length = 182, width = 218, area;
+    area = length*width;
+    cout << "The area of this rectangle is " << area;
+}
