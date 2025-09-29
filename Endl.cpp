3453e2d1b8d8749e1c45bbbe7fa3f7137c3dd131
Ayush <ayush.kundesar@gmail.com>
2025-09-29 09:54:27 +0530

    Add files via upload

---
+++ Endl.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << 7 << endl;
+    cout << 3 << endl;
+}
---
+++ No_Endl.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+//To print two different outputs in same line
+int main(){
+    cout << 11*9;
+    cout << 11/9;
+}
